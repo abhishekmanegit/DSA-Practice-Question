eb5df3151c0056f1abb2dae4e946523f0abc347e
Abhishek Mane <abhishekmaneam20@gmail.com>
2026-05-01 14:47:54 +0530

    Valid Anagram

---
+++ ValidAnagram.java
@@ -0,0 +1,37 @@
+/*
+Problem : Valid Anagram
+
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+*/
+
+class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+        
+         int m = s.length();
+         int n = t.length();
+
+         if(m!=n){
+
+            return false;
+         }
+         else{
+            int count[] = new int[26];
+
+            for(int i=0;i<m;i++){
+                count[s.charAt(i) - 'a']++;
+
+            }
+            for(int i=0;i<n;i++){
+                count[s.charAt(i)-'a']--;
+            }
+
+            for(int i = 0;i<count.length;i++){
+                if(count[i]!=0){
+                    return false;
+                }
+            }
+         }
+         return true;
+    
+    }
+}
