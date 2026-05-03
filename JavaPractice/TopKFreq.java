c71caed076a195ec4efacdb622df995c0d6ead4c
Abhishek Mane <abhishekmaneam20@gmail.com>
2026-05-03 21:31:51 +0530

    Added the Description Of Problem

--- JavaPractice/TopKFreq.java
+++ JavaPractice/TopKFreq.java
@@ -1,3 +1,19 @@
+/*
+Top K Frequent Elements
+
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+ 
+
+Sample Test Case :
+
+Input: nums = [1,1,1,2,2,3], k = 2
+
+Output: [1,2]
+
+
+*/
+
 class TopKFreq {
     public int[] topKFrequent(int[] nums, int k) {
 
