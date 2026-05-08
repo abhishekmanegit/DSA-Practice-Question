aaa8d94c839c4dd67afb9f1aa48883b156d8e054
Abhishek Mane <abhishekmaneam20@gmail.com>
2026-05-08 12:22:44 +0530

    New Problem Solved

---
+++ JavaPractice/Angles.java
@@ -0,0 +1,26 @@
+class Angles {
+    public double[] internalAngles(int[] sides) {
+
+        double a = sides[0];
+        double b = sides[1];
+        double c = sides[2];
+
+        if(a+b <= c || b+c <= a || a+c <= b){
+            return new double[0];
+        }
+
+        double A = Math.acos((b*b + c*c - a*a)/(2 * b * c));
+        double B = Math.acos((a*a + c*c - b*b)/(2 * a * c));
+        double C = Math.acos((a*a + b*b - c*c)/(2 * a * b));
+
+        A = Math.toDegrees(A);
+        B = Math.toDegrees(B);
+        C = Math.toDegrees(C);
+
+        double[] angles = {A,B,C};
+        Arrays.sort(angles);
+        return angles;
+
+        
+    }
+}
---
+++ JavaPractice/ClosestTarget.java
@@ -0,0 +1,16 @@
+class ClosestTarget {
+    public int getMinDistance(int[] nums, int target, int start) {
+
+        int dist = Integer.MAX_VALUE;
+
+        for(int i = 0;i < nums.length;i++){
+
+           if(nums[i] == target)
+            {
+                dist = Math.min(Math.abs(i-start), dist);
+            }
+
+        }
+        return dist;
+    }
+}
