b40111d756416d4bbd0a87f3be64e50d91ddf2a7
Abhishek Mane <abhishekmaneam20@gmail.com>
2026-06-16 20:37:03 +0530

    Add string reversal practice

--- Others/src/StringBuild.java
+++ Others/src/StringBuild.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
 public class StringBuild {
+
+public static void main(String [] args){
+
+    StringBuilder sb = new StringBuilder("Abhishek");
+
+    for(int i = 0;i<sb.length()/2;i++){
+        int front = i;
+        int back = sb.length()-1-i;
+
+        char frontChar = sb.charAt(front);
+        char backChar = sb.charAt(back);
+
+        sb.setCharAt(front, backChar);
+        sb.setCharAt(back, frontChar);
+
+
+    }
+    System.out.println(sb);
+
+}
 }
