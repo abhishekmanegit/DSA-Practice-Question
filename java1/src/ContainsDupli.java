d99c6ff05215f34f257c5ca9ebaf217ac0c5a294
Abhishek Mane <abhishekmaneam20@gmail.com>
2026-06-19 23:53:14 +0530

    Contains Duplicate Problem Solved

---
+++ java1/src/ContainsDupli.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class ContainsDupli {
+    public static boolean contain(int[] nums){
+
+        Arrays.sort(nums);
+
+        for(int i = 0;i<nums.length-1;i++){
+            if(nums[i] == nums[i + 1]) return true;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args){
+
+        int[] nums = {1 ,3,4,1};
+
+        System.out.println(contain(nums));
+    }
+}
