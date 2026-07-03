eabd2895fd0c669871f046646a3fa41ed9e02557
Abhishek Mane <abhishekmaneam20@gmail.com>
2026-07-03 21:40:26 +0530

    Day 7 DSA

---
+++ java1/src/DSAByHarsh/Day7/SubSeqStr.java
@@ -0,0 +1,15 @@
+package DSAByHarsh.Day7;
+
+public class SubSeqStr {
+
+    public static void main(String[] args) {
+        String str = "abc";
+        char[] result = new char[str.length()];
+
+        for(int i = 0;i<str.length();i++){
+            char temp = str[i];
+
+
+        }
+    }
+}
